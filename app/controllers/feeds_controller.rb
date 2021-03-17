646d0d44117ce1301437aeccb8c87f0a4001502e
Austin Adams <caustinwadams@gmail.com>
2021-03-17 16:53:59 -0400

    Added create feed page

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -14,25 +14,28 @@ class FeedsController < ApplicationController
   end
 
   def create
-    @feed = Feed.new
-    @feed.title = params[:title]
-    @feed.url = params[:url]
+    @feed = Feed.new(feed_params)
 
     begin
       old_feed = open(@feed.url, :http_basic_authentication=>["austin11793","Manwax12!"]).read
-      @feed.new_feed = old_feed
-      i_start = 1
-      i_end   = 1
+      @feed.new_feed = old_feed.gsub("&amp;", "&")
 
-      while i_start and i_end
-        i_start = (old_feed =~ /<item>/m)
-        i_end   = (old_feed =~ /<\/item>/m) + 6
+      if @feed.save
+        flash[:success] = "Feed added"
+      else
+        flash[:danger] = "URL opened but feed not saved"
       end
     rescue
-
+      flash[:danger] = "Bad credentials to open GaS feed."
     ensure
-
+      redirect_to feeds_url
     end
   end
+
+  private
+
+    def feed_params
+      params.require(:feed).permit(:title, :url)
+    end
 
 end
